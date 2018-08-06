0de3f535d0a0f669025b51f8e9c8a21eaa34eee4
James Funderburk <jfunderb@dialog6.rrz.uni-koeln.de>
2018-08-07 00:45:34 +0200

    revise dictinfowhich to autodetect whether on Cologne website

--- dictinfowhich.php
+++ dictinfowhich.php
@@ -1,4 +1,14 @@
 <?php
- $dictinfowhich = "xampp";
- #$dictinfowhich = "cologne"; 
+ /* dictinfowhich is used so we know how to construct
+    paths to various files.  There are assumed to be two
+    file structures: one like that at Cologne sanskrit-lexicon-uni-koeln.de
+    web site, and another simpler structure based on that devised for 
+    use on other servers, such as xampp or a typical Linux php setup.
+*/
+ $dir = dirname(__DIR__);
+ if (preg_match('/afs/',$dir)) {
+  $dictinfowhich = "cologne"; 
+ }else {
+  $dictinfowhich = "xampp";
+ }
 ?>
