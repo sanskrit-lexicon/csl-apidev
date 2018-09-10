bc0b4f970b093f37d521e7d11b102937ffa52c99
funderburkjim <funderburkjim@gmail.com>
2018-09-10 16:12:36 -0400

    improve 'not found' message

--- getword.php
+++ getword.php
@@ -49,11 +49,13 @@ function getword_html($getParms,$matches) {
  $dbg=false;
  $nmatches = count($matches);
  dbgprint($dbg,"getword.php #3: nmatches=$nmatches\n");
+ $key = $getParms->key;
+ $keyin = $getParms->keyin1;
  if ($nmatches == 0) {
   //echo "DBG: cmd1 = $cmd1\n";
  $meta = '<meta charset="UTF-8">';
   echo "$meta\n";
-  echo "<h2>not found: '$key'</h2>\n";
+  echo "<h2>not found: '$keyin' (slp1 = $key)</h2>\n";
   return;
  }
 
@@ -77,10 +79,12 @@ function getword_html_raw($getParms,$matches) {
  $dbg = false;
  $meta = '<meta charset="UTF-8">';
  $nmatches = count($matches);
+ $key = $getParms->key;
+ $keyin = $getParms->keyin1;
  if ($nmatches == 0) {
   echo "$meta\n";
   $key = $getParms->key;
-  echo "<h2>not found: '$key'</h2>\n";
+  echo "<h2>not found: '$keyin' (slp1= $key)</h2>\n";
   exit;
  }
  
