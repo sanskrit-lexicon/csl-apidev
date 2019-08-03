d5333f3272ef354fb479bedda8e214ac93c9eddd
James Funderburk <jfunderb@dialog6.rrz.uni-koeln.de>
2019-08-03 02:21:32 +0200

    Remove old and definitely unneeded files

--- mw_webtc1api/old/listhierskip.php
+++
@@ -1,6 +0,0 @@
-<?php
-// dummy routine, always return False
-function listhierskip_data($data2) {
- return False;
-}
-?>
--- mw_webtc1api/unused_disphier.php
+++
@@ -1,6 +0,0 @@
-<?php
-///web/webtc1/disphier.php
-require_once("disphier_main.php");
-disphier_main();
-
-?>
