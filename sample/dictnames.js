e24dada95b90a229243416b0a44db0e4df4f6f82
funderburkjim <funderburkjim@gmail.com>
2021-04-10 20:34:04 -0400

    Reorder dictnames list. Ref: https://github.com/sanskrit-lexicon/COLOGNE/issues/344

--- sample/dictnames.js
+++ sample/dictnames.js
@@ -1,54 +1,52 @@
 var CologneDisplays = {};
 CologneDisplays.dictionaries= {
     dictnames:
-[['WIL' , 'Wilson Sanskrit-English Dictionary'],
-['YAT' , 'Yates Sanskrit-English Dictionary'],
-['GST' , 'Goldstücker Sanskrit-English Dictionary'],
-['BEN' , 'Benfey Sanskrit-English Dictionary'],
-['MW72' , 'Monier-Williams Sanskrit-English Dictionary'],
+[ /*['AP' , 'Practical Sanskrit-English Dictionary'],*/
 ['AP90' , 'Apte Practical Sanskrit-English Dictionary'],
-['LAN' , 'Lanman Sanskrit Reader Dictionary'],
+['ARMH' , 'Abhidhānaratnamālā of Halāyudha'],
+['BEN' , 'Benfey Sanskrit-English Dictionary'],
+['BHS' , 'Edgerton Buddhist Hybrid Sanskrit Dictionary'],
+['BOP' , 'Bopp Glossarium Sanscritum'],
+['BOR' , 'Borooah English-Sanskrit Dictionary'],
+['BUR' , 'Burnouf Dictionnaire Sanscrit-Français'],
 ['CAE' , 'Cappeller Sanskrit-English Dictionary'],
+['CCS' , 'Cappeller Sanskrit Wörterbuch'],
+['GRA' , 'Grassmann Wörterbuch zum Rig Veda'],
+['GST' , 'Goldstücker Sanskrit-English Dictionary'],
+['IEG' , 'Indian Epigraphical Glossary'],
+['INM' , 'Index to the Names in the Mahabharata'],
+['KRM' , 'Kṛdantarūpamālā'],
+['LAN' , 'Lanman Sanskrit Reader Dictionary'],
+['MCI' , 'Mahabharata Cultural Index'],
 ['MD' , 'Macdonell Sanskrit-English Dictionary'],
 ['MW' , 'Monier-Williams Sanskrit-English Dictionary'],
-['SHS' , 'Shabda-Sagara Sanskrit-English Dictionary'],
-['BHS' , 'Edgerton Buddhist Hybrid Sanskrit Dictionary'],
-['AP' , 'Practical Sanskrit-English Dictionary, revised edition'],
-['PD' , 'An Encyclopedic Dictionary of Sanskrit on Historical Principles'],
+['MW72' , 'Monier-Williams Sanskrit-English Dictionary'],
 ['MWE' , 'Monier-Williams English-Sanskrit Dictionary'],
-['BOR' , 'Borooah English-Sanskrit Dictionary'],
-['AE' , 'Apte Student English-Sanskrit Dictionary'],
-['BUR' , 'Burnouf Dictionnaire Sanscrit-Français'],
-['STC' , 'Stchoupak Dictionnaire Sanscrit-Français'],
-['PWG' , 'Böhtlingk and Roth Grosses Petersburger Wörterbuch'],
-['GRA' , 'Grassmann Wörterbuch zum Rig Veda'],
-['PW' , 'Böhtlingk Sanskrit-Wörterbuch in kürzerer Fassung'],
-['CCS' , 'Cappeller Sanskrit Wörterbuch'],
-['SCH' , 'Schmidt Nachträge zum Sanskrit-Wörterbuch'],
-['BOP' , 'Bopp Glossarium Sanscritum'],
-['SKD' , 'Sabda-kalpadruma'],
-['VCP' , 'Vacaspatyam'],
-['INM' , 'Index to the Names in the Mahabharata'],
-['VEI' , 'The Vedic Index of Names and Subjects'],
-['PUI' , 'The Purana Index'],
-['ACC' , 'Aufrecht Catalogus Catalogorum'],
-['KRM' , 'Kṛdantarūpamālā'],
-['IEG' , 'Indian Epigraphical Glossary'],
-['SNP' , 'Meulenbeld Sanskrit Names of Plants'],
+/*['PD' , 'An Encyclopedic Dictionary of Sanskrit on Historical Principles'],*/
 ['PE' , 'Puranic Encyclopedia'],
 ['PGN' , 'Personal and Geographical Names in the Gupta Inscriptions'],
-['MCI' , 'Mahabharata Cultural Index'],
-['ARMH','Abhidhānaratnamālā of Halāyudha']
+['PUI' , 'The Purana Index'],
+['PW' , 'Böhtlingk Sanskrit-Wörterbuch in kürzerer Fassung'],
+['PWG' , 'Böhtlingk and Roth Grosses Petersburger Wörterbuch'],
+['SCH' , 'Schmidt Nachträge zum Sanskrit-Wörterbuch'],
+['SHS' , 'Shabda-Sagara Sanskrit-English Dictionary'],
+['SKD' , 'Sabda-kalpadruma'],
+['SNP' , 'Meulenbeld Sanskrit Names of Plants'],
+['STC' , 'Stchoupak Dictionnaire Sanscrit-Français'],
+['VCP' , 'Vacaspatyam'],
+['VEI' , 'The Vedic Index of Names and Subjects'],
+['YAT' , 'Yates Sanskrit-English Dictionary'],
+['WIL' , 'Wilson Sanskrit-English Dictionary']
 ],
     dictshowMake: function() {
-	var x = [];
+ var x = [];
         var i,y,label,value,obj;
         for(i=0;i<this.dictnames.length;i++) {
             var y = this.dictnames[i];
-	    obj = {label: y[0]+" " + y[1], value:y[0]};
+     obj = {label: y[0]+" " + y[1], value:y[0]};
             x.push(obj);
-	}
-	return x;
+ }
+ return x;
     },
     
 };
